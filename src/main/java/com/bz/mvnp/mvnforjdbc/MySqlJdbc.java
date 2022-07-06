fdec0a84cd8d79e8bffd4973b4a32683f890a7f3
Chetan Patil <piyushp0541@gmail.com>
2022-07-06 18:43:15 +0530

    retrive using city or state

--- src/main/java/com/bz/mvnp/mvnforjdbc/MySqlJdbc.java
+++ src/main/java/com/bz/mvnp/mvnforjdbc/MySqlJdbc.java
@@ -14,10 +14,18 @@ public class MySqlJdbc {
 		try {
 			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/AddressBookService", "root", "root");
 //			Statement stmt = connect.createStatement();
-			PreparedStatement pStmt = connect.prepareStatement("delete from addressbook where firstname = ?");
-			String name ="Piyush";
-			pStmt.setString(1, name);
-			pStmt.execute();
+			String cityOrState ="Nashik";
+			PreparedStatement pStmt = connect.prepareStatement("select * from addressbook where city=? or state=?;");
+			pStmt.setString(1,cityOrState);
+			pStmt.setString(2,cityOrState);
+			ResultSet res = pStmt.executeQuery();
+			while(res.next()) {
+				System.out.println(res.getString(1)+" "+res.getString(2)+" "+res.getString(3)+" "+res.getString(4)+" "+res.getString(5)+" "+res.getInt(6)+" "+res.getInt(7)+" "+res.getString(8));
+			}
+				//	pStmt.execute();
+			
+			
+			
 		} catch (SQLException e) {
 			System.out.println("unable to connect");
 		} finally {
