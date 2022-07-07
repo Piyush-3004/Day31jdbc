603dee15eb8f82608bff253a589d5cd6750eaecd
Chetan Patil <piyushp0541@gmail.com>
2022-07-07 11:30:39 +0530

    identify book by name and type

--- src/main/java/com/bz/mvnp/mvnforjdbc/MySqlJdbc.java
+++ src/main/java/com/bz/mvnp/mvnforjdbc/MySqlJdbc.java
@@ -4,30 +4,119 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
 
+//import java.sql.*;
 public class MySqlJdbc {
 
 	public static void main(String[] args) throws SQLException {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(
+				"Enter 1 to get elements sorted on firstnames of persons in City, 2 to identify by name and type");
+		int ch = scanner.nextInt();
+		if (ch == 1)
+			sortByName();
+		if (ch == 2)
+			identifyByNameAndType();
+
+	}
+
+	public static void identifyByNameAndType() throws SQLException {
+
 		Connection connect = null;
 		try {
 			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/AddressBookService", "root", "root");
-//			Statement stmt = connect.createStatement();
-			String city ="Nashik";
-			PreparedStatement pStmt = connect.prepareStatement("select * from addressbook where city=? order by firstname;");
-			pStmt.setString(1,city);
+
+			Statement stmt = connect.createStatement();
+
+			// Create colunm named type assign type as friend ////////////
+//			stmt.execute("alter table addressbook add type varchar(50);");
+
+			// Create colunm named name assign book as book1 ////////////
+//			stmt.execute("alter table addressbook add name varchar(50);");
+
+//			stmt.execute("update addressbook set type='Friend' where firstname = 'Piyush';");			
+
+//			stmt.execute("update addressbook set name='Book1' where firstname = 'Piyush';");
+			// stmt.execute();
+
+			PreparedStatement pStmt = connect.prepareStatement("update addressbook set type=? where city = 'Nashik';");
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("enter firstname to update type ");
+			String name = scanner.next();
+			pStmt.setString(1, name);
+			System.out.println("enter type for " + name);
+			String type = scanner.next();
+			pStmt.setString(2, type);
 			ResultSet res = pStmt.executeQuery();
-			while(res.next()) {
-				System.out.println(res.getString(1)+" "+res.getString(2)+" "+res.getString(3)+" "+res.getString(4)+" "+res.getString(5)+" "+res.getInt(6)+" "+res.getInt(7)+" "+res.getString(8));
+
+			while (res.next()) {
+				System.out.println(res.getString(1) + " " + res.getString(2) + " " + res.getString(3) + " "
+						+ res.getString(4) + " " + res.getString(5) + " " + res.getInt(6) + " " + res.getInt(7) + " "
+						+ res.getString(8));
 			}
-				//	pStmt.execute();
-			
+
+			System.out.println("enter book name to view the book");
+			String bookName = scanner.next();
+			PreparedStatement pStmt1 = connect.prepareStatement("select * from addressbook where name=?;");
+			pStmt1.setString(1, bookName);
+			ResultSet res1 = pStmt1.executeQuery();
+
+			while (res1.next()) {
+				System.out.println(res1.getString(1) + " " + res1.getString(2) + " " + res1.getString(3) + " "
+						+ res1.getString(4) + " " + res1.getString(5) + " " + res1.getInt(6) + " " + res1.getInt(7)
+						+ " " + res1.getString(8) + " " + res1.getString(9) + " " + res1.getString(10));
+			}
+
+			System.out.println("enter book type to view the book");
+			String bookType = scanner.next();
+			PreparedStatement pStmt2 = connect.prepareStatement("select * from addressbook where type=?;");
+			pStmt2.setString(1, bookType);
+			ResultSet res2 = pStmt2.executeQuery();
+
+			while (res2.next()) {
+				System.out.println(res2.getString(1) + " " + res2.getString(2) + " " + res2.getString(3) + " "
+						+ res2.getString(4) + " " + res2.getString(5) + " " + res2.getInt(6) + " " + res2.getInt(7)
+						+ " " + res2.getString(8) + " " + res2.getString(9) + " " + res2.getString(10));
+			}
+
 		} catch (SQLException e) {
 			System.out.println("unable to connect");
 		} finally {
 			connect.close();
 		}
+
+	}
+
+	public static void sortByName() throws SQLException {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter city to sort by name");
+		scanner.next();
+		Connection connect = null;
+		try {
+			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/AddressBookService", "root", "root");
+//			Statement stmt = connect.createStatement();
+			String city = "Nashik";
+			PreparedStatement pStmt = connect
+					.prepareStatement("select * from addressbook where city=? order by firstname;");
+			pStmt.setString(1, city);
+			ResultSet res = pStmt.executeQuery();
+			while (res.next()) {
+				System.out.println(res.getString(1) + " " + res.getString(2) + " " + res.getString(3) + " "
+						+ res.getString(4) + " " + res.getString(5) + " " + res.getInt(6) + " " + res.getInt(7) + " "
+						+ res.getString(8));
+			}
+			// pStmt.execute();
+
+		} catch (SQLException e) {
+			System.out.println("unable to connect");
+		} finally {
+			connect.close();
+		}
+
 	}
 
 }
